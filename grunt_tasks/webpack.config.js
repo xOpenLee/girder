6b0b0e3ffffebc6fd8751a2a811169b9af11fad7
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-05-17 14:25:54 -0400

    Initial treeview plugin

--- grunt_tasks/webpack.config.js
+++ grunt_tasks/webpack.config.js
@@ -143,7 +143,7 @@ module.exports = {
             // PNG, JPEG
             {
                 resource: {
-                    test: /\.(png|jpg)$/,
+                    test: /\.(png|jpg|gif)$/,
                     include: loaderPathsNodeModules
                 },
                 use: [
