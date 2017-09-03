8ba7de391f18f1556031ae2eab3c80594e20eb40
Pierre Assemat <pierre.assemat@kitware.com>
2017-09-03 02:39:30 -0400

    Store Dicom files metadata into the Dicom item

--- plugins/dicom_viewer/server/__init__.py
+++ plugins/dicom_viewer/server/__init__.py
@@ -46,6 +46,101 @@ class DicomItem(Resource):
                 f['dicom'] = dicom
         return files
 
+    # @access.public(scope=TokenScope.DATA_READ)
+    # @autoDescribeRoute(
+    #     Description('Get and store DICOM metadata, if any, for all files in the item.')
+    #     .modelParam('id', 'The item ID',
+    #                 model='item', level=AccessType.READ, paramType='path')
+    #     .errorResponse('ID was invalid.')
+    #     .errorResponse('Read permission denied on the item.', 403)
+    # )
+    # def storeMetaData(self, item):
+    #     # The goal is to store extract common tag of dicom files
+    #     # to store them into the dicom item as metadata
+    #     item['dicomMeta'] = {}
+    #     files = list(ModelImporter.model('item').childFiles(item))
+    #     for i, f in enumerate(files):                     # EXTRACT METADATA
+    #         f['dicom'] = parse_file(f)
+    #         files[i] = f
+    #     # filter out non-dicom files
+    #     files = [x for x in files if x.get('dicom')]
+    #     # sort files
+    #     files = sorted(files, key=sort_key)
+    #     for i, f in files:
+    #         metadata[i] = f['dicom']
+    #         ### SORT COMMON AND NON COMMON
+
+
+def _1extractMetadata(item):
+    item['dicomMeta'] = {}
+    item['dicomFiles'] = []
+
+    for file in ModelImporter.model('item').childFiles(item):
+        if not isDicom(file):
+            continue
+        fileMeta = parse_file(file)
+        item['dicomFiles'].append({
+            'fileId': file['_id'],
+            'meta': fileMeta
+        })
+    return item
+
+def isDicom(file):
+    if file.get('dicom'):
+        return True
+    return False
+
+def _2extractCommonMetadata(item):          # MARCHE PAS ENCORE
+    # find the common data
+    old_k = []
+    old_v  = []
+    metadata = []
+    uniqueMetadata = []
+    commonMetadata = []
+    uniqueListMetadata = []
+    OK = False
+    for file in item['dicomFiles']:
+        meta = file['meta']
+        # FIRST FILE AS REFERENCE
+        if not old_k and not old_v:
+            for k, v in meta.iteritems():
+                old_k.append(k)
+                old_v.append(v)
+        else:
+            for k, v in meta.iteritems():
+                lgtV = len(commonMetadata)
+                for j in old_v:
+                    if j == v:
+                        commonMetadata.append({k:v})
+                if lgtV == len(commonMetadata):
+                    uniqueMetadata.append({k:v})
+            uniqueListMetadata.append(uniqueMetadata)
+            uniqueMetadata = []
+            # if OK == True:
+            #     break
+            # else:
+            #     OK = True
+    # Store common tags and values
+    item['dicomMeta'] = setListObject(commonMetadata)
+    print '\n--> COMMON META\n', item['dicomMeta']
+    # Store list of different tags per files
+    item['dicomFiles'] = uniqueListMetadata
+    print '\n--> UNIQUE META\n', item['dicomFiles']
+
+    return item
+
+def setListObject(listObject):
+    # Delete all the redundante object from the list
+    for i in range(0,len(listObject)-1):
+        removeObject = []
+        for j in range(i+1,len(listObject)):
+            if listObject[i] == listObject[j]:
+                removeObject.append(j)
+        for k in range(0,len(removeObject)):
+            listObject.pop(removeObject[k])
+            for h in range(0,len(removeObject)):
+                removeObject[h] -= 1
+    return listObject
 
 def sort_key(f):
     dicom = f.get('dicom', {})
@@ -117,3 +212,5 @@ def load(info):
     dicomItem = DicomItem()
     info['apiRoot'].item.route(
         'GET', (':id', 'dicom'), dicomItem.getDicom)
+    # info['apiRoot'].item.route(
+    #     'POST', (':id', 'parseDicom'), dicomItem.storeMetaData)
